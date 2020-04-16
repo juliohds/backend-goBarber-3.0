852abfc7a7ebe589fc4b856ce4f3f0082f49f107
Julio Henrique <juliohenrique@Julios-MacBook-Air.local>
2020-04-16 20:45:03 -0300

    upload photo with multer

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -20,7 +23,7 @@ usersRouter.post('/', async (request, response) => {
   }
 })
 
-usersRouter.patch('/avatar', ensureAuthenticated, async (ruquest, response) => {
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (ruquest, response) => {
   return response.json({ ok: true })
 })
 
