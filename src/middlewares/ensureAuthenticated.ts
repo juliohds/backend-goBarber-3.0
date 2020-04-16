bd74c3c1f8479d9a56737f259ac04935ea1df5bc
Julio Henrique <juliohenrique@Julios-MacBook-Air.local>
2020-04-16 20:36:14 -0300

    gitkeep

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -2,6 +2,12 @@ import { Request, Response, NextFunction} from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function ensureAuthenticated(request: Request, response: Response, next: NextFunction): void {
   const authHeader = request.headers.authorization;
   if(!authHeader){
@@ -11,7 +17,12 @@ export default function ensureAuthenticated(request: Request, response: Response
   const [, token] = authHeader.split(' ');
   try {
     const decoded =  verify(token, authConfig.jwt.secret);
-    console.log(decoded);
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
     return next();
   } catch {
     throw new Error('Invalid JWT token ')
